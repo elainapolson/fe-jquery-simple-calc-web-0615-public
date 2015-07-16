8586f137dc5d35d6af7484f22a5b8d82f624a8ac
elainapolson <elainapolson@gmail.com>
2015-07-16 15:25:14 -0400

    done..maybe should refactor

--- js/simple-calc.js
+++ js/simple-calc.js
@@ -1,5 +1,63 @@
 $(function(){
 
-  // write your solution here.
+  var num1 = 0;
+  var num2 = 0;
+  var operation = '+';
+
+//changing operation
+  $('#add').click(function() {
+    $('#operation').text('+');
+    operation = '+';
+  });
+
+  $('#sub').click(function() {
+    $('#operation').text('-');
+    operation = '-';
+  });
+
+  $('#mult').click(function() {
+    $('#operation').text('*');
+    operation = '*';
+  });
+
+  $('#div').click(function() {
+    $('#operation').text('/');
+    operation = '/';
+  });
+
+// changing num1 and num2
+  $('#number1 > button.incr').first().click(function() {
+    $('#number1 > h2').text(num1 += 1);
+  });
+
+  $('#number2 > button.incr').first().click(function() {
+    $('#number2 > h2').text(num2 += 1);
+  });
+
+  $('#number1 > button.decr').first().click(function() {
+    $('#number1 > h2').text(num1 -= 1);
+  });
+
+  $('#number2 > button.decr').first().click(function() {
+    $('#number2 > h2').text(num2 -= 1);
+  });
+
+// perform operation
+  function operate() {
+    if (operation == '+') { 
+      return num1 + num2
+    } else if (operation == '-') {
+      return num1 - num2
+    } else if (operation == '*') {
+      return num1 * num2
+    } else if (operation == '/') {
+      return num1 / num2
+    }
+    
+  }
+
+  $('#equals').click(function() {
+    $('#result').text(operate);
+  });
 
 });
